807f0725513e47406715bb8600b9fb0a8bd10a46
Mehdi Saber <m1d3s@protonmail.com>
2021-07-19 15:48:17 +0200

    Added Weather API

---
+++ plugins/esgi-weather/weather.php
@@ -0,0 +1,20 @@
+<?php
+
+$apiKey = "2e144d72721a5fea8f59e8a4300e551e";
+$cityName = "London";
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" . $cityName . "&lang=en&units=metric&APPID=" . $apiKey;
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+var_dump($data);
