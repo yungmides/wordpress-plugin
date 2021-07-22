0e88c31b1599985d732c02beb89f4d4f6baff2d7
Alexmdz77 <alext77165@gmail.com>
2021-07-22 11:13:33 +0200

    remove widget inputs

--- wp-content/plugins/esgi-weather/esgi-weather.php
+++ wp-content/plugins/esgi-weather/esgi-weather.php
@@ -39,13 +39,15 @@ function run_weather() {
     $plugin->run();
 }
 
+run_weather();
+
 class esgi_widget extends WP_Widget
 {
     // Set up the widget name and description.
     public function __construct()
     {
-        $widget_options = array('classname' => 'esgi_widget', 'description' => 'Un plugin de météo par le groupe 7.');
-        // parent::__construct('esgi_widget', 'ESGI Widget', $widget_options);
+        $widget_options = array('classname' => 'esgi_weather', 'description' => 'Un plugin de météo par le groupe 7.');
+        parent::__construct('esgi_weather', 'ESGI Weather', $widget_options);
     }
 
 
@@ -60,19 +62,11 @@ class esgi_widget extends WP_Widget
         $backgroundColor = $instance['backgroundColor'];
         $widgetWidth = $instance['widgetWidth'];
         $textColor = $instance['textColor'];
-        $days = $instance['days'];
-        $showSunrise = $instance['showSunrise'];
-        $showWind = $instance['showWind'];
-        $showCurrent = $instance['showCurrent'];
 
         echo '<div class="esgi_widget weather_widget_wrap"
                  data-text-color="' . $textColor . '"
                  data-background="' . $backgroundColor . '"
                  data-width="' . $widgetWidth . '"
-                 data-days="' . $days . '"
-                 data-sunrise="' . $showSunrise . '"
-                 data-wind="' . $showWind . '"
-                 data-current="' . $showCurrent . '"
                  data-city="' . $city . '"
                  data-country="' . $country . '">
     
@@ -98,39 +92,18 @@ class esgi_widget extends WP_Widget
             $widgetWidth = '100';
         }
 
-        if (isset($instance['days'])) {
-            $days = $instance['days'];
-        } else {
-            $days = 3;
-        }
-
-
-        if (isset($instance['showSunrise'])) {
-            $showSunrise = $instance['showSunrise'];
-        } else {
-            $showSunrise = "";
-        }
-
-        if (isset($instance['showWind'])) {
-            $showWind = $instance['showWind'];
-        } else {
-            $showWind = "";
-        }
-
-        $showCurrent = !empty($instance['showCurrent']) ? $instance['showCurrent'] : 'on';
-
         ?>
-        <div class="tiempo_form">
+        <div class="esgi_weather_form">
             <div class="form-section">
-                <h3>Location</h3>
+                <h3>Localisation</h3>
                 <div class="form-line">
-                    <label class="text-label" for="<?php echo $this->get_field_id('city'); ?>">City:</label>
+                    <label class="text-label" for="<?php echo $this->get_field_id('city'); ?>">Ville :</label>
                     <input type="text" id="<?php echo $this->get_field_id('city'); ?>"
                            name="<?php echo $this->get_field_name('city'); ?>"
                            value="<?php echo esc_attr($city); ?>"/>
                 </div>
                 <div class="form-line">
-                    <label class="text-label" for="<?php echo $this->get_field_id('country'); ?>">Country:</label>
+                    <label class="text-label" for="<?php echo $this->get_field_id('country'); ?>">Pays :</label>
                     <input type="text" id="<?php echo $this->get_field_id('country'); ?>"
                            name="<?php echo $this->get_field_name('country'); ?>"
                            value="<?php echo esc_attr($country); ?>"/>
@@ -138,86 +111,22 @@ class esgi_widget extends WP_Widget
             </div>
 
             <div class="form-section">
-                <h3>Weather Data</h3>
-                <div class="form-line">
-                    <input type="checkbox"
-                        <?php if ($showCurrent == 'on') {
-                            echo 'checked';
-                        }; ?>
-                           id="<?php echo $this->get_field_id('showCurrent'); ?>"
-                           name="<?php echo $this->get_field_name('showCurrent'); ?>"/>
-                    <label for="<?php echo $this->get_field_id('showCurrent'); ?>">Show: Current weather</label>
-                </div>
-                <div class="form-line">
-                    <input type="checkbox"
-                        <?php if ($showWind == 'on') {
-                            echo 'checked';
-                        }; ?>
-                           id="<?php echo $this->get_field_id('showWind'); ?>"
-                           name="<?php echo $this->get_field_name('showWind'); ?>"/>
-                    <label for="<?php echo $this->get_field_id('showWind'); ?>">Show: Chance for rain, Wind and
-                        Humidity</label>
-                </div>
-                <div class="form-line">
-                    <input type="checkbox"
-                        <?php if ($showSunrise == 'on') {
-                            echo 'checked';
-                        }; ?>
-                           id="<?php echo $this->get_field_id('showSunrise'); ?>"
-                           name="<?php echo $this->get_field_name('showSunrise'); ?>"/>
-                    <label for="<?php echo $this->get_field_id('showSunrise'); ?>">Show: Sunrise and sunset time</label>
-                </div>
-            </div>
-            <div class="form-section">
-                <h3>Daily Forecast</h3>
-                <div class="form-line">
-                    <select name="<?php echo $this->get_field_name('days'); ?>">
-                        <option value="0" <?php if ($days == 0) {
-                            echo 'selected';
-                        } ?>>No Daily Forecast
-                        </option>
-                        <option value="2" <?php if ($days == 2) {
-                            echo 'selected';
-                        } ?>>2 Days
-                        <option value="3" <?php if ($days == 3) {
-                            echo 'selected';
-                        } ?>>3 Days
-                        </option>
-                        <option value="4" <?php if ($days == 4) {
-                            echo 'selected';
-                        } ?>>4 Days
-                        </option>
-                        <option value="5" <?php if ($days == 5) {
-                            echo 'selected';
-                        } ?>>5 Days
-                        </option>
-                        <option value="6" <?php if ($days == 6) {
-                            echo 'selected';
-                        } ?>>6 Days
-                        </option>
-                    </select>
-                </div>
-            </div>
-
-
-            <div class="form-section">
-                <h3>Look & Feel</h3>
+                <h3>Style</h3>
 
                 <div class="form-line">
-                    <label for="<?php echo $this->get_field_id('backgroundColor'); ?>">Background Color
-                        (optional):</label>
+                    <label for="<?php echo $this->get_field_id('backgroundColor'); ?>">Couleur de fond (optionnel) :</label>
                     <input type="color" id="<?php echo $this->get_field_id('backgroundColor'); ?>"
                            name="<?php echo $this->get_field_name('backgroundColor'); ?>"
                            value="<?php echo esc_attr($backgroundColor); ?>"/>
                 </div>
                 <div class="form-line">
-                    <label for="<?php echo $this->get_field_id('textColor'); ?>">Text Color (optional):</label>
+                    <label for="<?php echo $this->get_field_id('textColor'); ?>">Couleur du texte (optionnel) :</label>
                     <input type="color" id="<?php echo $this->get_field_id('textColor'); ?>"
                            name="<?php echo $this->get_field_name('textColor'); ?>"
                            value="<?php echo esc_attr($textColor); ?>"/>
                 </div>
                 <div>
-                    <div class="widget-width-line"><label for="<?php echo $this->get_field_id('widgetWidth'); ?>">Widget Stretch (width):</label>
+                    <div class="widget-width-line"><label for="<?php echo $this->get_field_id('widgetWidth'); ?>">Taille du widget :</label>
                     </div>
                     <div class="form-line">
                         <input type="radio" id="<?php echo $this->get_field_id('widgetWidth'); ?>"
@@ -231,7 +140,7 @@ class esgi_widget extends WP_Widget
                                 echo 'checked';
                             }; ?>
                                name="<?php echo $this->get_field_name('widgetWidth'); ?>"
-                               value="tight"/> Tight as possible
+                               value="tight"/> Le plus petit possible
                     </div>
                 </div>
             </div>
@@ -255,19 +164,6 @@ class esgi_widget extends WP_Widget
         $instance['backgroundColor'] = sanitize_hex_color(strip_tags($new_instance['backgroundColor']));
         $instance['textColor'] = sanitize_hex_color(strip_tags($new_instance['textColor']));
         $instance['widgetWidth'] = sanitize_text_field(strip_tags($new_instance['widgetWidth']));
-        $instance['showSunrise'] = sanitize_text_field($new_instance['showSunrise']);
-        $instance['showWind'] = sanitize_text_field($new_instance['showWind']);
-        $instance['showCurrent'] = sanitize_text_field($new_instance['showCurrent']);
-        $instance['days'] = sanitize_text_field(strip_tags($new_instance['days']));
-        if ($new_instance['showSunrise'] != "on") {
-            $instance['showSunrise'] = "false";
-        }
-        if ($new_instance['showWind'] != "on") {
-            $instance['showWind'] = "false";
-        }
-        if ($new_instance['showCurrent'] != "on") {
-            $instance['showCurrent'] = "false";
-        }
         return $instance;
     }
 }
@@ -279,5 +175,3 @@ function jpen_register_esgi_widget()
 }
 
 add_action('widgets_init', 'jpen_register_esgi_widget');
-
-run_weather();
