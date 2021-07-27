407e8b463267ab942a4be2c63346fa95077de33d
Mehdi Saber <m1d3s@protonmail.com>
2021-07-27 17:06:41 +0200

    Error handling

--- wp-content/plugins/esgi-weather/js/plugin.js
+++ wp-content/plugins/esgi-weather/js/plugin.js
@@ -14,16 +14,32 @@ document.addEventListener('DOMContentLoaded', async function () {
     const title = document.createElement('h1');
     const temp = document.createElement('h2');
     const desc = document.createElement('p');
+    const error_element = document.createElement('p');
+    error_element.style.color = 'red';
+
     const data = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&units=metric&APPID=2e144d72721a5fea8f59e8a4300e551e`)
         .then((response) => response.json())
         .then((d) => {
-            title.innerHTML = d.name;
-            temp.innerHTML = `${d.main.temp}°C`;
-            desc.innerHTML = d.weather[0].description
-        })
-        .catch((e)=>console.error(e));
+            if (d.cod === 200) {
+                title.innerHTML = d.name;
+                temp.innerHTML = `${d.main.temp}°C`;
+                desc.innerHTML = d.weather[0].description;
+                esgi_widget.append(title, temp, desc);
+            }
+            else if (d.cod === 404) {
+                error_element.innerHTML = `La ville ${city} n'existe pas`;
+                esgi_widget.append(error_element);
+            }
+            else {
+                error_element.innerHTML = `Une erreur inconnue s'est produite : ${d.message}` ;
+                esgi_widget.append(error_element);
+            }
 
-    esgi_widget.append(title, temp, desc);
+        })
+        .catch((e)=> {
+            error_element.innerHTML = e;
+            esgi_widget.append(error_element);
+        });
 
 
 
