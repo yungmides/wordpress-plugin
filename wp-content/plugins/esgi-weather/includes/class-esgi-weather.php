e8216a3995a1a9b9d661d3ff7fbf777dfee4dd36
Mehdi Saber <m1d3s@protonmail.com>
2021-07-28 17:00:31 +0200

    début settings page

--- wp-content/plugins/esgi-weather/includes/class-esgi-weather.php
+++ wp-content/plugins/esgi-weather/includes/class-esgi-weather.php
@@ -14,10 +14,48 @@ class ESGI_Weather{
                 __( 'ESGI Weather', 'textdomain' ),
                 'manage_options',
                 'weather.php',
-                'weather'
+                'weather_page'
             );
         }
+
+        function weather_page() {
+            ?>
+            <h1>ESGI Weather Settings</h1>
+            <form action="options.php" method="post">
+                <?php
+                settings_field('esgi_weather_options');
+                do_setting_sections('esgi_weather_plugin');
+                ?>
+                <?php submit_button(); ?>
+            </form>
+
+            <?php
+        }
+
+        function esgi_weather_register_settings() {
+            register_setting('esgi_weather_options', 'esgi_weather_options', 'esgi_weather_options_validate');
+            add_settings_section('api_settings', 'API Settings', 'esgi_weather_section_text', 'esgi_weather_plugin');
+            add_settings_field( 'esgi_weather_setting_city', 'City', 'esgi_weather_setting_city', 'esgi_weather_plugin', 'api_settings' );
+            // Ajouter de la clé API ? Mais risque de sécurité
+        }
+
+        function esgi_weather_section_text() {
+            echo '<p>Here you can insert cities for the Weather plugin</p>';
+        }
+
+        function esgi_options_validate($input) {
+            $newinput['city'] = trim($input['city']);
+            $newinput['city'] = ucwords($newinput['city']);
+            return $newinput;
+        }
+
+        function esgi_weather_city() {
+            $options = get_option('esgi_weather_options');
+            echo "<input id='esgi_weather_setting_city' name='esgi_weather_options[city]' type='text' value='" . esc_attr( $options['city'] ) . "' />";
+        }
+
         add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
+        add_action('admin_init', 'esgi_weather_register_settings');
     }
     
 
